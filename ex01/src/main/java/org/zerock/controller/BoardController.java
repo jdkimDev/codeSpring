678df40f56a5497b13b67670188053e2e2069f3a
JDKIM <JDKIM@JDKIM-PC>
2017-03-19 23:54:45 +0900

    3장

---
+++ ex01/src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,20 @@
+package org.zerock.controller;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.service.BoardService;
+
+@Controller
+@RequestMapping("/board/*")
+public class BoardController {
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
+	@Inject
+	private BoardService service;
+	
+	
+}
