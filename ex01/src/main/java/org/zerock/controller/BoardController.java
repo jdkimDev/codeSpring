e8d6b34fcbb8d58f6a2de13d25f09de3d61ddc3c
JDKIM <JDKIM@JDKIM-PC>
2017-04-17 01:01:35 +0900

    챕터2 6장 삭제/수정처리

--- ex01/src/main/java/org/zerock/controller/BoardController.java
+++ ex01/src/main/java/org/zerock/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.BoardVO;
 import org.zerock.service.BoardService;
@@ -45,4 +46,37 @@ public class BoardController {
 	
 	}
 	
+	@RequestMapping(value = "/read", method = RequestMethod.GET)
+	public void read(@RequestParam("bno") int bno, Model model) throws Exception {
+		//source typing 이 줄어든다는거 말고 이점이 있는가? 명시해주는게 더좋지 않나? 이리도 사용한다고만 생각.
+		//model.addAttribute("boardVO", service.read(bno));
+		model.addAttribute(service.read(bno));
+	}
+	
+	
+	@RequestMapping(value = "/remove", method = RequestMethod.POST)
+	public String remove(@RequestParam("bno") int bno, RedirectAttributes rttr) throws Exception {
+		service.remove(bno);
+		rttr.addFlashAttribute("msg", "SUCCESS");
+		
+		return "redirect:/board/listAll";
+	}
+	
+	@RequestMapping(value = "/modify", method = RequestMethod.GET)
+	public void modifyGET(int bno, Model model) throws Exception {
+		model.addAttribute(service.read(bno));
+	}
+
+	@RequestMapping(value = "/modify", method = RequestMethod.POST)
+	public String modifyPOST(BoardVO board, RedirectAttributes rttr) throws Exception {
+		logger.info("mod post............");
+		service.modify(board);
+		rttr.addFlashAttribute("msg", "SUCCESS");
+		
+		return "redirect:/board/listAll";
+	}
+	
 }
+
+
+
